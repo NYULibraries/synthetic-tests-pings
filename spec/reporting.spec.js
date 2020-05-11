3d745a35948113cd5b2388d428033c6e30fc0217
Eric Griffis <eric.griffis@nyu.edu>
2020-05-11 09:35:23 -0400

    Add basic tests to reporting

--- spec/reporting.spec.js
+++ spec/reporting.spec.js
@@ -1,7 +1,10 @@
 const reporting = require('../lib/reporting');
 
 describe('sendMessage', () => {
-  it ('should return a string response', () => {
+  it('should return a string response', () => {
     expect(reporting.sendMessage('Test failed!')).toEqual('Send Test failed! to Slack!');
   });
+
+  test.todo('should send a message to slack API');
+  test.todo('should format its message based on parameters');
 });
