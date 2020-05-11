ee7c9ce18c2bbd4742fda731fc4a0bfeddc3b558
Eric Griffis <eric.griffis@nyu.edu>
2020-05-11 10:42:09 -0400

    Use non-nyu domains for testing

--- spec/syntheticTest.spec.js
+++ spec/syntheticTest.spec.js
@@ -3,47 +3,47 @@ const syntheticTest = require('../lib/syntheticTest');
 describe('testHttpResponse', () => {
   describe('for a successful response url', () => {
     it('should return true for a matching success status code', () => {
-      expect(syntheticTest.testHttpResponse({ url: 'https://library.nyu.edu/', status: 200, responseTime: 500 })).toBeTruthy();
+      expect(syntheticTest.testHttpResponse({ url: 'http://example.com/', status: 200, responseTime: 500 })).toBeTruthy();
     });
 
     it('should return false for response not received in specified time', () => {
-      expect(syntheticTest.testHttpResponse({ url: 'https://library.nyu.edu/', status: 200, responseTime: 1 })).toBeFalsy();
+      expect(syntheticTest.testHttpResponse({ url: 'http://example.com/', status: 200, responseTime: 1 })).toBeFalsy();
     });
 
     it('should return false for a wrong status code', () => {
-      expect(syntheticTest.testHttpResponse({ url: 'https://library.nyu.edu/', status: 201, responseTime: 500 })).toBeFalsy();
+      expect(syntheticTest.testHttpResponse({ url: 'http://example.com/', status: 201, responseTime: 500 })).toBeFalsy();
     });
   });
 
   describe('for a not found url', () => {
     it('should return true for a matching error status code', () => {
-      expect(syntheticTest.testHttpResponse({ url: 'https://library.nyu.edu/not/existing/path', status: 404, responseTime: 500 })).toBeTruthy();
+      expect(syntheticTest.testHttpResponse({ url: 'http://example.com/not/existing/path', status: 404, responseTime: 500 })).toBeTruthy();
     });
 
     test.todo('should return false for response not received in specified time');
 
     it('should return false for a wrong status code', () => {
-      expect(syntheticTest.testHttpResponse({ url: 'https://library.nyu.edu/not/existing/path', status: 500, responseTime: 500 })).toBeFalsy();
+      expect(syntheticTest.testHttpResponse({ url: 'http://example.com/not/existing/path', status: 500, responseTime: 500 })).toBeFalsy();
     });
   });
 
   describe('for a redirecting url', () => {
     it('should return true for the correct status code', () => {
-      expect(syntheticTest.testHttpResponse({ url: 'https://eshelf.library.nyu.edu/', status: 302, responseTime: 500 })).toBeTruthy();
+      expect(syntheticTest.testHttpResponse({ url: 'http://google.com/', status: 301, responseTime: 500 })).toBeTruthy();
     });
 
     test.todo('should return false for response not received in specified time');
 
     it('should return false for an incorrect status code', () => {
-      expect(syntheticTest.testHttpResponse({ url: 'https://eshelf.library.nyu.edu/', status: 301, responseTime: 500 })).toBeFalsy();
+      expect(syntheticTest.testHttpResponse({ url: 'http://google.com/', status: 302, responseTime: 500 })).toBeFalsy();
     });
 
     it('should return true for the correct status code and redirect location', () => {
       expect(syntheticTest.testHttpResponse({
-        url: 'https://eshelf.library.nyu.edu/',
-        status: 302,
+        url: 'http://google.com/',
+        status: 301,
         responseTime: 500,
-        redirectLocation: 'https://login.library.nyu.edu/login/passive?client_id=bbcf34b029d1b729dee3db58e6e1b13f469ae756c83d99b345dd1689fd6b36ef&return_uri=https%3A%2F%2Feshelf.library.nyu.edu%2F',
+        redirectLocation: 'https://www.google.com',
       })).toBeTruthy();
     });
 
@@ -51,11 +51,11 @@ describe('testHttpResponse', () => {
 
     it('should return false for the correct status code and incorrect redirect location', () => {
       expect(syntheticTest.testHttpResponse({
-        url: 'https://eshelf.library.nyu.edu/',
+        url: 'http://google.com',
         status: 302,
         responseTime: 500,
-        redirectLocation: 'https://login.library.nyu.edu/login/passive',
-      })).toBeTruthy();
+        redirectLocation: 'https://google.com',
+      })).toBeFalsy();
     });
   });
 });
