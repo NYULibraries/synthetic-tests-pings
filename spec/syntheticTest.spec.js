c7052f9d7d9d503da06efccbceb4a31a207a2d4b
Laura Nemeth <lauranemeth.pr@gmail.com>
2020-05-15 12:29:41 -0400

    Asynchronous testing to synthetic spec

--- spec/syntheticTest.spec.js
+++ spec/syntheticTest.spec.js
@@ -2,7 +2,7 @@ const syntheticTest = require('../lib/syntheticTest');
 
 describe('adapt', () => {
   it('identify and return http', () => {
-    expect(syntheticTest.adapt('http://example.com/')).toEqual(require("http"))
+    expect(syntheticTest.adapt('http://www.example.com/')).toEqual(require("http"))
   })
 })
 
@@ -28,91 +28,137 @@ describe('httpResponse', () => {
 describe('testHttpResponse', () => {
  describe('without a response url', () => {
     it('should throw an error', () => {
-      expect(() => syntheticTest.testHttpResponse()).toThrow();
-    });
+			expect.assertions(1);
+			return syntheticTest
+				.httpResponse()
+				.catch((e) => expect(e.message).toMatch('Received undefined'));
+		});
   });
 
   describe('for a successful response url', () => {
-    it('should return true for a matching success status code', () => {
-      expect(
-			 syntheticTest.testHttpResponse({
-					url: 'http://example.com/',
-					status: 200,
+    it('should return true for a matching success status code', async () => {
+     const test = await syntheticTest.testHttpResponse({
+				url: 'http:/www.example.com/',
+				status: 200,
+				responseTime: 500,
+			});
+      expect(test).toBe(true);
+    });
+
+    it('should return false for response not received in specified time', async () => {
+      const test = await syntheticTest.testHttpResponse({ 
+        url: 'http://www.example.com/', 
+        status: 200, 
+        responseTime: 1 
+        });
+        expect(test).toBe(false);
+    });
+
+    it('should return false for a wrong status code', async () => {
+			const test = await	syntheticTest.testHttpResponse({
+					url: 'http://www.example.com/',
+					status: 201,
 					responseTime: 500,
-				})
-			).toBeTruthy();
-    });
-
-    it('should return false for response not received in specified time', () => {
-      expect(syntheticTest.testHttpResponse({ url: 'http://example.com/', status: 200, responseTime: 1 })).toBeFalsy();
-    });
-
-    it('should return false for a wrong status code', () => {
-      expect(syntheticTest.testHttpResponse({ url: 'http://example.com/', status: 201, responseTime: 500 })).toBeFalsy();
+				});
+			expect(test).toBe(false);
     });
   });
 
   describe('for a not found url', () => {
-    it('should return true for a matching error status code', () => {
-      expect(syntheticTest.testHttpResponse({ url: 'http://example.com/not/existing/path', status: 404, responseTime: 500 })).toBeTruthy();
-    });
-    it('should return false for response not received in specified time', () => {
-      expect(syntheticTest.testHttpResponse({ url: 'http://example.com/not/existing/path', status: 404, responseTime: 1 })).toBeFalsy();
+    it('should return true for a matching error status code', async () => {
+      const test = await syntheticTest.testHttpResponse({ 
+          url: 'http://www.example.com/not/existing/path', 
+          status: 404, 
+          responseTime: 500 
+          });
+      expect(test).toBe(true);
     });
 
-    it('should return false for a wrong status code', () => {
-      expect(syntheticTest.testHttpResponse({ url: 'http://example.com/not/existing/path', status: 500, responseTime: 500 })).toBeFalsy();
+    it('should return false for response not received in specified time', async () => {
+      const test = await syntheticTest.testHttpResponse({
+					url: 'http://www.example.com/not/existing/path',
+					status: 404,
+					responseTime: 1,
+				});
+		  expect(test).toBe(false);
+    });
+
+    it('should return false for a wrong status code', async () => {
+			const test = await syntheticTest.testHttpResponse({
+					url: 'http://www.example.com/not/existing/path',
+					status: 500,
+					responseTime: 500,
+				});
+			expect(test).toBe(false);
     });
   });
 
   describe('for a redirecting url', () => {
-    it('should return true for the correct status code', () => {
-      expect(syntheticTest.testHttpResponse({ url: 'http://google.com/', status: 301, responseTime: 500 })).toBeTruthy();
+    it('should return true for the correct status code', async () => {
+      const test = await syntheticTest.testHttpResponse({ 
+        url: 'http://www.google.com/', 
+        status: 301, 
+        responseTime: 500 
+        });
+      expect(test).toBe(true);
     });
 
-    it('should return false for response not received in specified time', () => {
-      expect(syntheticTest.testHttpResponse({ url: 'http://google.com', status: 301, responseTime: 1 })).toBeFalsy();
+    it('should return false for response not received in specified time', async () => {
+      const test = await 	syntheticTest.testHttpResponse({
+					url: 'http://www.google.com',
+					status: 301,
+					responseTime: 1,
+				});
+			expect(test).toBe(false);
     });
 
 
-    it('should return false for an incorrect status code', () => {
-      expect(syntheticTest.testHttpResponse({ url: 'http://google.com/', status: 302, responseTime: 500 })).toBeFalsy();
+    it('should return false for an incorrect status code', async () => {
+      const test = await syntheticTest.testHttpResponse({
+					url: 'http://www.google.com/',
+					status: 302,
+					responseTime: 500,
+				});
+			expect(test).toBe(false);
     });
 
-    it('should return true for the correct status code and redirect location', () => {
-      expect(syntheticTest.testHttpResponse({
-        url: 'http://google.com/',
+    it('should return true for the correct status code and redirect location', async () => {
+      const test = await syntheticTest.testHttpResponse({
+        url: 'http://www.google.com/',
         status: 301,
         responseTime: 500,
         redirectLocation: 'https://www.google.com',
-      })).toBeTruthy();
+      });
+      expect(test).toBe(true);
     });
 
-    it('should return false with correct redirect location for response not received in specified time', () => {
-      expect(syntheticTest.testHttpResponse({
-        url: 'http://google.com/',
-        status: 301,
-        responseTime: 1,
-        redirectLocation: 'https://www.google.com',
-      })).toBeFalsy();
+    it('should return false with correct redirect location for response not received in specified time', async () => {
+      const test = await syntheticTest.testHttpResponse({
+					url: 'http://www.google.com/',
+					status: 301,
+					responseTime: 1,
+					redirectLocation: 'https://www.google.com',
+				});
+			expect(test).toBe(false);
     });
 
-    it('should return false for the correct status code and incorrect redirect location', () => {
-      expect(syntheticTest.testHttpResponse({
-        url: 'http://google.com',
-        status: 302,
-        responseTime: 500,
-        redirectLocation: 'https://google.com',
-      })).toBeFalsy();
+    it('should return false for the correct status code and incorrect redirect location', async () => {
+      const test = await syntheticTest.testHttpResponse({
+					url: 'http://www.google.com',
+					status: 302,
+					responseTime: 500,
+					redirectLocation: 'https://www.google.com',
+				});
+			expect(test).toBe(false);
     });
   });
 });
 
-syntheticTest.testHttpResponse =  jest.fn();
+//syntheticTest.testHttpResponse =  jest.fn();
 
 describe('checkUrl', () => {
   it('should call sendMessage when testHttpResponse returns false', ()=>{
-    expect(syntheticTest.checkUrl({ url: 'https://google.com', status: 201, responseTime: 1 })).toBe(false);
+    expect(syntheticTest.checkUrl({ url: 'https://www.google.com', status: 201, responseTime: 1 })).toBe(false);
     expect(syntheticTest.testHttpResponse).toHaveBeenCalledTimes(1);
   });
 
