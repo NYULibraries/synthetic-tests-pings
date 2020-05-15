f830644455f2f5c834ab6be23c7a4d00acbb3d85
Laura Nemeth <lauranemeth.pr@gmail.com>
2020-05-15 14:46:17 -0400

    checkUrl passes specs

--- spec/checkUrl.spec.js
+++ spec/checkUrl.spec.js
@@ -1,27 +1,27 @@
 const syntheticTest = require('../lib/syntheticTest');
 const reporting = require('../lib/reporting');
 
-reporting.sendMessage = jest.fn();
 
 describe('checkUrl', () => {
+    beforeEach(() => {
+        reporting.sendMessage = jest.fn();
+    })
+
   it('should call sendMessage when testHttpResponse returns false', async ()=>{
     const test = await syntheticTest.checkUrl({
-    url: 'https://www.google.com', 
-    status: 201, 
-    responseTime: 1 
+        url: 'https://www.google.com', 
+        status: 201, 
+        responseTime: 1 
     });
-    //expect(test).toBe(false);
-    expect(reporting.sendMessage).toHaveBeenCalledTimes(1);
-    //expect(test).toEqual(reporting.sendMessage);
+    expect(reporting.sendMessage).toHaveBeenCalled();
   });
 
   it('should not call sendMessage when testHttpResponse returns true', async ()=> {
     const test = await syntheticTest.checkUrl({ 
-      url: 'https://www.example.com', 
-      status: 200, 
-      responseTime: 500 
+         url: 'https://www.example.com', 
+        status: 200, 
+        responseTime: 500 
       });
-      //expect(test).toBe(true);
-      expect(reporting.sendMessage).toHaveBeenCalledTimes(0);
+      expect(reporting.sendMessage).not.toHaveBeenCalled()
   });
 });
