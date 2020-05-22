e7c88855e3bcbc4c9333e12c8c1a72effd894c55
Laura Nemeth <lauranemeth.pr@gmail.com>
2020-05-22 15:07:17 -0400

    Test sendMessage function

--- spec/reporting.spec.js
+++ spec/reporting.spec.js
@@ -18,19 +18,28 @@ describe("formatMessage", () => {
   it("should return accurate message for status trigger", () => {
     const testParams = { case: "status", actual: 201, expected: 200 };
     expect(reporting.formatMessage(testParams)).toEqual(
-      "Test failed! Unexpected status - Expected: 200, received: 201"
+      "Test failed! Unexpected status - Expected: 200, Received: 201"
     );
   });
 });
 
-describe("sendMessage", () => {
+describe("request", () => {
   it("should resolve the request with 200", async () => {
-    const res = {
-      status: 200,
-    };
+    const res = { status: 200 };
     axios.post.mockResolvedValue(res);
     return reporting
       .request({})
       .then((data) => expect(data.status).toEqual(200));
+  });
+});
+
+describe("sendMessage", () => {
+  beforeEach(() => {
+    reporting.request = jest.fn();
+  });
+  it("should call request with the same parameters", async () => {
+    const data = { case: "time", actual: 290, expected: 1 };
+    let test = await reporting.sendMessage(data);
+    expect(test).toBe(true);
   });
 });
