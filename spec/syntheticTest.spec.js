10a8abeb7e13b6e2b4c0929aa7ae461a6d028978
Laura Nemeth <lauranemeth.pr@gmail.com>
2020-05-28 10:23:28 -0400

    Move checkUrl specs to syntheticTest

--- spec/syntheticTest.spec.js
+++ spec/syntheticTest.spec.js
@@ -1,4 +1,5 @@
 const syntheticTest = require("../lib/syntheticTest");
+const reporting = require("../lib/reporting");
 
 describe("testHttpResponse", () => {
   describe("without a response url", () => {
@@ -121,4 +122,29 @@ describe("testHttpResponse", () => {
       expect(test).toMatchObject({ case: "relocation" });
     });
   });
+});
+
+
+describe('checkUrl', () => {
+	beforeEach(() => {
+		reporting.sendMessage = jest.fn();
+	});
+
+	it('should call sendMessage when testHttpResponse returns false', async () => {
+		const test = await syntheticTest.checkUrl({
+			url: 'https://www.google.com',
+			status: 201,
+			responseTime: 1,
+		});
+		expect(reporting.sendMessage).toHaveBeenCalled();
+	});
+
+	it('should not call sendMessage when testHttpResponse returns true', async () => {
+		const test = await syntheticTest.checkUrl({
+			url: 'https://www.example.com',
+			status: 200,
+			responseTime: 500,
+		});
+		expect(reporting.sendMessage).not.toHaveBeenCalled();
+	});
 });
