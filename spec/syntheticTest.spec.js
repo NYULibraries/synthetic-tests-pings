8f2abc1b0666bf25c33b1895aaea891f637d5edc
Laura Nemeth <lauranemeth.pr@gmail.com>
2020-06-01 12:06:03 -0400

    prettier

--- spec/syntheticTest.spec.js
+++ spec/syntheticTest.spec.js
@@ -124,27 +124,26 @@ describe("testHttpResponse", () => {
   });
 });
 
+describe("checkUrl", () => {
+  beforeEach(() => {
+    reporting.sendMessage = jest.fn();
+  });
 
-describe('checkUrl', () => {
-	beforeEach(() => {
-		reporting.sendMessage = jest.fn();
-	});
+  it("should call sendMessage when testHttpResponse returns false", async () => {
+    const test = await syntheticTest.checkUrl({
+      url: "https://www.google.com",
+      status: 201,
+      responseTime: 1,
+    });
+    expect(reporting.sendMessage).toHaveBeenCalled();
+  });
 
-	it('should call sendMessage when testHttpResponse returns false', async () => {
-		const test = await syntheticTest.checkUrl({
-			url: 'https://www.google.com',
-			status: 201,
-			responseTime: 1,
-		});
-		expect(reporting.sendMessage).toHaveBeenCalled();
-	});
-
-	it('should not call sendMessage when testHttpResponse returns true', async () => {
-		const test = await syntheticTest.checkUrl({
-			url: 'https://www.example.com',
-			status: 200,
-			responseTime: 500,
-		});
-		expect(reporting.sendMessage).not.toHaveBeenCalled();
-	});
+  it("should not call sendMessage when testHttpResponse returns true", async () => {
+    const test = await syntheticTest.checkUrl({
+      url: "https://www.example.com",
+      status: 200,
+      responseTime: 500,
+    });
+    expect(reporting.sendMessage).not.toHaveBeenCalled();
+  });
 });
