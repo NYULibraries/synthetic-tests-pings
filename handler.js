12777b3790cf9501bf20cf9b4506d9bab308a9c9
laurxnemeth <lauranemeth.pr@gmail.com>
2020-06-26 15:19:32 -0400

    Test redirection of eshelf, and change function accordingly

--- handler.js
+++ handler.js
@@ -7,6 +7,7 @@ module.exports.syntheticTest = async (event, context) => {
     url: process.env.TEST_URL,
     expectedStatus: process.env.EXPECTED_CODE,
     expectedResponseTime: process.env.EXPECTED_RESPONSE_TIME_MS,
+    expectedRedirectLocation: process.env.EXPECTED_REDIRECT_LOCATION,
   };
 
   let result = await checkUrl(testVariables);
