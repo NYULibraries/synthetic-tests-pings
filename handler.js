29621274c5ca9964a1e9423be0a95c6020c098bd
laurxnemeth <lauranemeth.pr@gmail.com>
2020-11-13 17:08:42 -0500

    Handler has everything necessary to work locally

--- handler.js
+++ handler.js
@@ -1,17 +1,30 @@
 "use strict";
+require('dotenv').config()
+const { checkUrl, pushToProm } = require("./lib/syntheticTest");
 
-const { checkUrl } = require("./lib/syntheticTest");
+const syntheticTest = async (event, context) => {
+  const appName = process.env.APP ? process.env.APP : "appNameNotProvided"
 
-module.exports.syntheticTest = async (event, context) => {
+  // const testVariables = {
+  //   url: process.env.TEST_URL,
+  //   expectedStatus: process.env.EXPECTED_CODE,
+  //   expectedResponseTime: process.env.EXPECTED_RESPONSE_TIME_MS,
+  //   expectedRedirectLocation: process.env.EXPECTED_REDIRECT_LOCATION,
+  //   //app: appName
+  // };
+
   const testVariables = {
-    url: process.env.TEST_URL,
-    expectedStatus: process.env.EXPECTED_CODE,
-    expectedResponseTime: process.env.EXPECTED_RESPONSE_TIME_MS,
-    expectedRedirectLocation: process.env.EXPECTED_REDIRECT_LOCATION,
-    app: process.env.APP
-  };
+    url: "https://bobcat.library.nyu.edu/this/is/a/test/please/ignore",
+    expectedStatus: 404,
+    expectedResponseTime: 1000,
+    app: appName
+  }
 
   let result = await checkUrl(testVariables);
+
+  if(appName !== "appNameNotProvided"){
+    await pushToProm(result.success, testVariables.app)
+  }
 
   return {
     statusCode: 200,
@@ -26,3 +39,5 @@ module.exports.syntheticTest = async (event, context) => {
     ),
   };
 };
+
+syntheticTest().then(e => console.log(e)).catch(err => console.error(err));
