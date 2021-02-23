9ef9b851f873734d0ac727e1ae46383e004071e5
laurxnemeth <lauranemeth.pr@gmail.com>
2021-02-23 14:10:57 -0500

    all tests functional

--- webdriver/primo.js
+++ webdriver/primo.js
@@ -1,13 +1,17 @@
-describe('My Login application', () => {
+describe("Primo search feature", () => {
   beforeEach(() => {
-    browser.url('https://bobcat.library.nyu.edu/primo-explore/search?vid=NYU');
+    browser.url("https://bobcat.library.nyu.edu/primo-explore/search?vid=NYU");
   });
-  it('should have a search bar', () => {
-    expect($('#searchBar')).toBeExisting();
+
+  it("should have a search bar", () => {
+    expect($("#searchBar")).toBeExisting();
   });
-//   it('should search for books successfully', () => {
-//     $('#searchBar').setValue("catcher in the rye");
-//     $('button[type="button"]').click();
-//     expect($('h3.item-title')).toContain('~{{');
-//   });
+
+  it("should search for books successfully", () => {
+    $("#searchBar").setValue("catcher in the rye");
+    $("button.button-confirm").click();
+
+    expect(browser).toHaveTitle("BobCat - catcher in the rye");
+    expect($("#searchResultPage1")).toHaveChildren();
+  });
 });
