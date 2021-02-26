1d2f1b89023155d8e3046b9192567b124c28ebc6
laurxnemeth <lauranemeth.pr@gmail.com>
2021-02-26 11:01:29 -0500

    Dockerfile for e2e created

--- wdio.conf.js
+++ wdio.conf.js
@@ -45,17 +45,18 @@ exports.config = {
   //
   capabilities: [
     {
-      // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-      // grid with only 5 firefox instances available you can make sure that not more than
-      // 5 instances get started at a time.
-      maxInstances: 5,
-      //
-      browserName: "chrome",
+      maxInstances: 1,
+      browserName: 'chrome',
       acceptInsecureCerts: true,
-      // If outputDir is provided WebdriverIO can capture driver session logs
-      // it is possible to configure which logTypes to include/exclude.
-      // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-      // excludeDriverLogs: ['bugreport', 'server'],
+      'goog:chromeOptions': {
+          args: [
+              '--no-sandbox',
+              '--disable-infobars',
+              '--headless',
+              '--disable-gpu',
+              '--window-size=1440,735'
+          ],
+      }
     },
   ],
   //
