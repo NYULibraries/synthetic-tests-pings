2f1f5a0c4adae7510eb03930f434c46529898729
laurxnemeth <lauranemeth.pr@gmail.com>
2021-03-15 12:58:43 -0400

    first test

---
+++ e2e/test.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'Get title': (browser) => {
+    browser
+      .url('https://bobcat.library.nyu.edu/primo-explore/search?vid=NYU')
+      .setValue('#searchBar', 'catcher in the rye')
+      .click('button.button-confirm')
+      .assert.title('BobCat - catcher in the rye');
+  }
+}
